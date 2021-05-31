fa92f35a21b056f757d2fa5e23a83da01b2ca42f
vuthanhdanh080200 <vuthanhdanh080200@gmail.com>
2021-05-31 17:24:38 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var mqtt = require("mqtt");
+var url = "mqtt://io.adafruit.com";
+
+var username = "lengochieu6102";
+var password = "aio_pZbT27oegv3sSBoFmBTyJ0KTUFZg";
+
+var client = mqtt.connect(url, { username: username, password: password });
+
+var topic = "lengochieu6102/feeds/bk-iot-button";
+client.on("connect", function () {
+  console.log("connected");
+  client.subscribe(topic, function (err) {
+    if (!err) {
+      console.log("subscribed");
+      client.publish(topic, "HAHA");
+    }
+  });
+});
+
+client.on("message", function (topic, message) {
+  console.log(message.toString());
+});

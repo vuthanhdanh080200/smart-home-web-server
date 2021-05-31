6518097edc89e0727dd188917a512f1198d59bf0
vuthanhdanh080200 <vuthanhdanh080200@gmail.com>
2021-05-31 18:09:52 +0700

    testtest

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+var http = require("http");
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write("Hello World!");
+    res.end();
+  })
+  .listen(8080);
+
 var mqtt = require("mqtt");
 var url = "mqtt://io.adafruit.com";
 
